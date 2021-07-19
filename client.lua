f2be317ef58bd780897396491f2ca482f3ccfd6c
ahq <81720579+Hackerman69999@users.noreply.github.com>
2021-07-19 17:54:09 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,26 @@
+function ShowNotification( text )
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(false, false)
+end
+
+RegisterCommand('car', function(source, args, rawCommand)
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
+    local vehicle = args[1]
+    if vehicle == nil then veh = "br8" end
+    vehiclehash = GetHashKey(vehicle)
+    RequestModel(vehiclehash)
+    
+    Citizen.CreateThread(function() 
+        local waiting = 0
+        while not HasModelLoaded(vehiclehash) do
+            waiting = waiting + 100
+            Citizen.Wait(100)
+            if waiting > 2500 then
+                ShowNotification("Could not load the vehicle.")
+                break
+            end
+        end
+        CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
+    end)
+end)
